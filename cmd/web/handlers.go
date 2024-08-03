d4e44d72e76608e8b67a26ffcc21e7a9e60f3107
Duy-B-Nguyen <duybnguyen04@gmail.com>
2024-08-03 02:55:21 -0400

    serve html templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
-package main 
+package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -20,7 +22,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Snippetbox"))
+	// // Initialize a slice containing the paths to the two files. Note that the
+	// home.page.tmpl file must be the *first* file in the slice.
+	files := []string {
+		"../../ui/html/home.page.tmpl",
+		"../../ui/html/base.layout.tmpl",
+		"../../ui/html/footer.partial.tmpl",
+	}
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we can pass the slice of file p
+	// as a variadic parameter
+	// file path that you pass to the template.ParseFiles() function
+	// must either be relative to your current working directory, or an absolute path
+	ts,err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	// We then use the Execute() method on the template set to write the templa
+	// content as the response body. The last parameter to Execute() represents
+	// dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
---
+++ ui/src/routes/Home/Home.jsx
@@ -0,0 +1,18 @@
+import "./Home.scss"
+
+const Home = () => {
+   return (
+      <div className="home-container">
+         <title>Home - Snippetbox</title>
+         <div className="content-container">
+            <h2>LATEST SNIPPETS</h2>
+            <div className="content">
+               <p>THERE'S NOTHING TO SEE HERE YET</p>
+            </div>
+
+         </div>
+      </div>
+   )
+}
+
+export default Home
