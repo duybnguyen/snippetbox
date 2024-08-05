ef6484c9dd0a9ecd6a4d547cff5a2459b9ebdcab
Duy Nguyen <duybnguyen04@gmail.com>
2024-08-05 02:22:26 -0400

    add centralized error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,12 +22,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Importantly, we then return from the handler. If we don't return the hand
 	// would keep executing and also write the "Hello from SnippetBox" message.
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) //implemented in helpers.go
 		return
 	}
 	// // Initialize a slice containing the paths to the two files. Note that the
 	// home.page.tmpl file must be the *first* file in the slice.
-	files := []string {
+	files := []string{
 		"../../ui/html/home.page.tmpl",
 		"../../ui/html/base.layout.tmpl",
 		"../../ui/html/footer.partial.tmpl",
@@ -37,10 +37,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// as a variadic parameter
 	// file path that you pass to the template.ParseFiles() function
 	// must either be relative to your current working directory, or an absolute path
-	ts,err := template.ParseFiles(files...)
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) //implemented in helpers.go
 		return
 	}
 	// We then use the Execute() method on the template set to write the templa
@@ -49,10 +49,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	err = ts.Execute(w, nil)
 	if err != nil {
 		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) //implemented in helpers.go
 	}
 }
-
 
 // Change the signature of the showSnippet handler so it is defined as a method
 // against *application.
@@ -65,7 +64,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	// not found response.
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) //implemented in helpers.go
 		return
 	}
 	// Use the fmt.Fprintf() function to interpolate the id value with our response
@@ -73,7 +72,6 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 	w.Write([]byte("Display a specific snippet..."))
 }
-
 
 // Change the signature of the createSnippet handler so it is defined as a method
 // against *application.
@@ -92,7 +90,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Allow", "POST")
 		// w.WriteHeader(405)
 		// w.Write([]byte("Method Not Allowed"))
-		http.Error(w, "Method Not Allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) //implemented in helpers.go
 		return
 	}
 
